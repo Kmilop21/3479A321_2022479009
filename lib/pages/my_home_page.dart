f38790d69be9d45f18eb8530ee155c6b73dec6c0
Camilo <camilo.e.provoste.p@gmail.com>
2024-09-04 00:30:41 -0400

    my_home_page file with widget

---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger(
+  printer: PrettyPrinter(),
+);
+
+var loggerNoStack = Logger(
+  printer: PrettyPrinter(methodCount: 0),
+);
+
+void demo() {
+  logger.d('Log message with 2 methods');
+
+  loggerNoStack.i('Info message');
+
+  loggerNoStack.w('Just a warning');
+
+  logger.e('Error! Something bad happened', error: 'Test Error');
+
+  loggerNoStack.t({'key': 5, 'value': 'something'});
+
+  Logger(printer: SimplePrinter(colors: true)).t('boom');
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+
+      logger.i('counter increased, new count: $_counter');
+    });
+  }
+
+  void _decreaseCounter() {
+    setState(() {
+      _counter--;
+      logger.i('counter decreased, new count: $_counter');
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+      logger.i('counter reseted, new count: $_counter');
+    });
+  }
+
+  List<Widget> _createPersistentButtons() {
+    return [
+      ElevatedButton(
+          onPressed: _decreaseCounter, child: const Icon(Icons.remove)),
+      ElevatedButton(onPressed: _resetCounter, child: const Icon(Icons.refresh))
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    logger.i('MyHomePage is loading');
+
+    String stonksAsset = 'assets/icons/stonks.svg';
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // TRY THIS: Try changing the color here to a specific color (to
+          // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+          // change color while the other colors stay the same.
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            //
+            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+            // action in the IDE, or press "p" in the console), to see the
+            // wireframe for each widget.
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                stonksAsset,
+                semanticsLabel: 'stonks',
+                width: 30,
+                height: 30,
+              ),
+              const Text(
+                'Has persionado el boton estas veces:',
+              ),
+              Text(
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: const Icon(Icons.delete),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
+        persistentFooterButtons: _createPersistentButtons());
+  }
+}
